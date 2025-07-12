5b78c3d2a6b6c869b28897d281c0ce1a48b0fa7f
alauddinapex <md.alauddin@apexproperty.com.bd>
2025-07-12 20:24:49 +0600

    just added

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,7 +2,10 @@ import { cookies } from 'next/headers';
 
 export const getServerSession = () => {
   // In a real app, you would verify the session token here
-  const cookieStore = cookies();
-  const sessionToken = cookieStore?.get('sessionToken');
-  return sessionToken ? { user: { id: '1', username: 'john_doe' } } : null;
+  // const cookieStore = cookies();
+  // const sessionToken = cookieStore?.get('sessionToken');
+  // return sessionToken ? { user: { id: '1', username: 'john_doe' } } : null;
+
+  return { user: { id: '1', username: 'john_doe' } }
+
 };
